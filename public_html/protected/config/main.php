692aec0824a4d7fe2d3871bbbec20f6cbe222230
HiNa <hina@bouhime.com>
2013-03-04 20:55:51 +0900

    oops

--- public_html/protected/config/main.php
+++ public_html/protected/config/main.php
@@ -5,31 +5,31 @@
 // This is the main Web application configuration. Any writable
 // CWebApplication properties can be configured here.
 return array(
-	'basePath' => dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-	'name' => 'おけいはんなう',
-    'lang' => 'ja',
+    'basePath' => dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
+    'name' => 'おけいはんなう',
+    'language' => 'ja',
 
-	// preloading 'log' component
-	'preload'=>array('log'),
+    // preloading 'log' component
+    'preload'=>array('log'),
 
-	// autoloading model and component classes
-	'import'=>array(
-		'application.models.*',
-		'application.components.*',
-	),
+    // autoloading model and component classes
+    'import'=>array(
+        'application.models.*',
+        'application.components.*',
+    ),
 
-	'modules'=>array(
-	),
+    'modules'=>array(
+    ),
 
-	// application components
-	'components'=>array(
-		'user'=>array(
-			'allowAutoLogin' => false,
-		),
-		'urlManager'=>array(
+    // application components
+    'components'=>array(
+        'user'=>array(
+            'allowAutoLogin' => false,
+        ),
+        'urlManager'=>array(
             'showScriptName' => false,
-			'urlFormat'=>'path',
-			'rules'=>array(
+            'urlFormat'=>'path',
+            'rules'=>array(
                 ''                                                  => 'train/index',
                 'station'                                           => 'train/stations',
                 'station/<id:\d+>/timetable/<day:holiday|weekday>'  => 'train/stationDia',
@@ -38,14 +38,14 @@ return array(
                 'station/near'                                      => 'train/stationNear',
                 'train/<id:\d+>'                                    => 'train/trainDia',
                 'tweet'                                             => 'train/tweet',
-				'<controller:\w+>/<action:\w+>'                     => '<controller>/<action>',
-			),
-		),
-		'db'=>array(
+                '<controller:\w+>/<action:\w+>'                     => '<controller>/<action>',
+            ),
+        ),
+        'db'=>array(
             'connectionString' => 'pgsql:host=localhost;port=5432;dbname=keihan',
             'username' => 'fetus',
             'password' => 'fetus',
-		),
+        ),
         'viewRenderer' => array(
             'class' => 'application.extensions.yiiext.renderers.smarty.ESmartyViewRenderer',
             'fileExtension' => '.tpl',
@@ -55,30 +55,30 @@ return array(
             ),
             'pluginsDir' => 'application.smartyPlugins',
         ),
-		'errorHandler'=>array(
-			// use 'site/error' action to display errors
-			'errorAction'=>'site/error',
-		),
-		'log'=>array(
-			'class'=>'CLogRouter',
-			'routes'=>array(
-				array(
-					'class'=>'CFileLogRoute',
-					'levels'=>'error, warning',
-				),
+        'errorHandler'=>array(
+            // use 'site/error' action to display errors
+            'errorAction'=>'site/error',
+        ),
+        'log'=>array(
+            'class'=>'CLogRouter',
+            'routes'=>array(
+                array(
+                    'class'=>'CFileLogRoute',
+                    'levels'=>'error, warning',
+                ),
                 // array(
                 //     'class'=>'CFileLogRoute',
                 //     'levels'=>'trace',
                 //     'categories'=>'system.db.*',
                 //     'logFile'=>'sql.log',
                 // ),
-			),
-		),
-	),
+            ),
+        ),
+    ),
 
-	// application-level parameters that can be accessed
-	// using Yii::app()->params['paramName']
-	'params'=>array(
-		'adminEmail'=>'hina@jp3cki.jp',
-	),
+    // application-level parameters that can be accessed
+    // using Yii::app()->params['paramName']
+    'params'=>array(
+        'adminEmail'=>'hina@jp3cki.jp',
+    ),
 );
