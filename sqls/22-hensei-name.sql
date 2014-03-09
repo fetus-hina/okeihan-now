655d2d46372c4b8fdf6c58cd5657a00fd5c1f6ca
fetus <fetus@sumika.single-quote.com>
2014-03-09 15:30:19 +0900

    nickname

--- sqls/22-hensei-name.sql
+++ sqls/22-hensei-name.sql
@@ -5,3 +5,14 @@
 UPDATE "formations"
     SET "nickname" = 'THOMAS&FRIENDS'
     WHERE "id" IN (3006, 10006);
+
+UPDATE "formations" SET "nickname" = NULL WHERE "nickname" IS NOT NULL;
+UPDATE "formations" SET "nickname" = 'きかんしゃトーマス号' WHERE "id" IN ( 10006, 3001 );
+UPDATE "formations" SET "nickname" = 'きかんしゃパーシー号' WHERE "id" = 10001;
+UPDATE "formations" SET "nickname" = 'いなり、こんこん、恋いろは。(ウカ) #いなこん ' WHERE "id" = 1506;
+UPDATE "formations" SET "nickname" = 'いなり、こんこん、恋いろは。(丸太町) #いなこん ' WHERE "id" = 2455;
+UPDATE "formations" SET "nickname" = 'いなり、こんこん、恋いろは。(三条) #いなこん ' WHERE "id" = 2634;
+UPDATE "formations" SET "nickname" = 'いなり、こんこん、恋いろは。(丹波橋) #いなこん ' WHERE "id" = 5554;
+UPDATE "formations" SET "nickname" = 'いなり、こんこん、恋いろは。(いなり) #いなこん ' WHERE "id" = 6004;
+UPDATE "formations" SET "nickname" = 'いなり、こんこん、恋いろは。(墨染) #いなこん ' WHERE "id" = 7203;
+
