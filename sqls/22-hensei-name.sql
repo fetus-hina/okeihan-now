dad39652561f382a6546993b2273a6796b8a75ce
AIZAWA Hina <hina@bouhime.com>
2014-05-07 15:15:10 +0900

    ヘッドマーク更新（いなこん削除、たまこラブストーリー追加）

--- sqls/22-hensei-name.sql
+++ sqls/22-hensei-name.sql
@@ -1,9 +1,3 @@
 UPDATE "formations" SET "nickname" = NULL WHERE "nickname" IS NOT NULL;
-UPDATE "formations" SET "nickname" = 'いなり、こんこん、恋いろは。(うか様) #いなこん' WHERE "id" = 1506;
-UPDATE "formations" SET "nickname" = 'いなり、こんこん、恋いろは。(丸太町) #いなこん' WHERE "id" = 2455;
-UPDATE "formations" SET "nickname" = 'いなり、こんこん、恋いろは。(三条) #いなこん' WHERE "id" = 2634;
-UPDATE "formations" SET "nickname" = 'いなり、こんこん、恋いろは。(丹波橋) #いなこん' WHERE "id" = 5554;
-UPDATE "formations" SET "nickname" = 'いなり、こんこん、恋いろは。(いなり) #いなこん' WHERE "id" = 6004;
-UPDATE "formations" SET "nickname" = 'いなり、こんこん、恋いろは。(墨染) #いなこん' WHERE "id" = 7203;
-
 UPDATE "formations" SET "nickname" = '宇治・伏見ギャラリートレイン' WHERE "id" = 10005;
+UPDATE "formations" SET "nickname" = 'たまこラブストーリーHM' WHERE "id" IN ( 7001, 7002 );
