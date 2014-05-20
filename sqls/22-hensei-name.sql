25766adbf85529b117dca4de125c35a0336f6f81
AIZAWA Hina <hina@bouhime.com>
2014-05-20 12:23:54 +0900

    13021F,13022F,HM

--- sqls/22-hensei-name.sql
+++ sqls/22-hensei-name.sql
@@ -1,3 +1,2 @@
 UPDATE "formations" SET "nickname" = NULL WHERE "nickname" IS NOT NULL;
 UPDATE "formations" SET "nickname" = '宇治・伏見ギャラリートレイン' WHERE "id" = 10005;
-UPDATE "formations" SET "nickname" = 'たまこラブストーリーHM' WHERE "id" IN ( 7001, 7002 );
