f0731eafcea1148aec77cc5130216203beb5b9db
AIZAWA Hina <hina@bouhime.com>
2014-06-16 14:21:10 +0900

    いなこんHM終了

--- sqls/22-hensei-name.sql
+++ sqls/22-hensei-name.sql
@@ -1,3 +1,2 @@
 UPDATE "formations" SET "nickname" = NULL WHERE "nickname" IS NOT NULL;
 UPDATE "formations" SET "nickname" = '宇治・伏見ギャラリートレイン' WHERE "id" = 10005;
-UPDATE "formations" SET "nickname" = 'いなり、こんこん、恋いろは。HM #いなこん' WHERE "id" IN ( 2226, 2624, 6001 );
